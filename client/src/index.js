2569c7f432b37da555a2dde871c2326005b97841
Jack <verploegjack@gmail.com>
2021-02-24 16:51:23 -0700

    npm install jest and npx eslint --init

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-const App = () => {
-   return <h1>This is our React App</h1>;
- }
+const App = () => {return (<h1>This is our React App</h1>)};
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- config.js
+++ config.js
@@ -1,1 +1,17 @@
+// All this is doing is inserting the parse API keys into every $.ajax
+// request that you make so you don't have to.
 
+// Put your parse application keys here!
+// $.ajaxPrefilter(function (settings, _, jqXHR) {
+//   jqXHR.setRequestHeader('Authorization', 'd84d59f5ddfc76351566595985ee5e0793cc8ac4');
+// });
+
+// Put your campus prefix here
+window.CAMPUS = 'bld15';
+
+//  GET /products/:product_id
+
+// Put your YouTube API keys here!
+// var YOUTUBE_API_KEY = 'YOUR_API_KEY_HERE';
+
+// export default YOUTUBE_API_KEY;
