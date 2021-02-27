b0f44d278225720960fd667067a12e562c416007
Jack <verploegjack@gmail.com>
2021-02-27 10:19:40 -0700

    skeleton structure set up for app.  each widget component jsx file made so each member can work on their part

--- client/src/components/ProductOverview.jsx
+++ client/src/components/ProductOverview.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import bulma from 'bulma';
 
 // COMPONENT
-const RatingsReviews = (props) => (
+const ProductOverview = (props) => (
   <div>
     <button type="button" className="button is-primary">TEST BUTTON</button>
     <section className="hero is-primary is-fullheight">
       <div className="hero-body">
         <div className="container has-text-centered">
-          <h1 className="title">Hello, Ryne!</h1>
-          <h2 className="subtitle">This Ratings and reviews</h2>
+          <h1 className="title">Hello, Jack!</h1>
+          <h2 className="subtitle">This is Product Overview</h2>
         </div>
       </div>
     </section>
     <div className="tile is-grey-dark">
-      <h3>TESTING3</h3>
+      <h3>TESTING</h3>
     </div>
   </div>
 );
 
 // EXPORTS
-export default RatingsReviews;
+export default ProductOverview;
