7bdd3122355fa72f2ac48a2eaa04e34e968aa93c
Ryne Schroder <ryne2010@hotmail.com>
2021-02-26 17:54:21 -0700

    added RatingsReview component and store2 library

---
+++ client/src/components/RatingsReviews.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button } from "react-bulma-components";
+import axios from 'axios';
+
+// COMPONENT
+const RatingsReviews = (props) => (
+  <div>
+    <Button color="danger" size="large">TEST BUTTON</Button>
+  </div>
+);
+
+// EXPORTS
+export default RatingsReviews;
