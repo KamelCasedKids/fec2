69fc11863613c872e6ad822a76554b214aaf7884
Jack <verploegjack@gmail.com>
2021-03-01 17:19:49 -0700

    added image placeholder

--- client/src/components/overview/ProductOverview.jsx
+++ client/src/components/overview/ProductOverview.jsx
@@ -4,30 +4,34 @@ import bulma from 'bulma';
 // COMPONENT
 const ProductOverview = (props) => (
   <div>
-    <div className="tile is-ancestor">
-      <div className="tile is-vertical">
-        <div className="tile">
+    <section className="hero is-grey-dark is-fullheight">
+      <div className="tile is-ancestor is-10 has-text-centered is-centered">
+        <div className="tile is-vertical">
+          <div className="tile">
+            <div className="tile is-parent">
+              <article className="tile is-child box">
+                Image Gallery
+                <img src="https://bulma.io/images/placeholders/640x480.png" alt="" />
+              </article>
+            </div>
+            <div className="tile is-parent is-vertical">
+              <article className="tile is-child box">
+                Product Details
+              </article>
+              <article className="tile is-child box">
+                Shopping Cart Options
+              </article>
+            </div>
+          </div>
           <div className="tile is-parent">
             <article className="tile is-child box">
-              Image Gallery
+              Product Description
             </article>
           </div>
-          <div className="tile is-parent is-vertical">
-            <article className="tile is-child box">
-              Product Details
-            </article>
-            <article className="tile is-child box">
-              Shopping Cart Options
-            </article>
-          </div>
-        </div>
-        <div className="tile is-parent">
-          <article className="tile is-child box">
-            Product Description
-          </article>
         </div>
       </div>
-    </div>
+    </section>
+    <section className="section space" />
   </div>
 );
 
